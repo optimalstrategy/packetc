1ce789af74677491601e2b36a3fafd25f89581f3
jprochazk <honza.spacir@gmail.com>
2020-12-30 03:20:22 +0100

    implement better error output

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -8,18 +8,22 @@ pub mod ast;
 pub mod parser;
 
 fn pretty_error(file: &str, err: ParseError<LineCol>) -> String {
-    let token = file.chars().nth(err.location.offset).unwrap_or('\0');
-    format!(
-        "Unexpected token '{}' at line {}, column {}, expected one of {}",
-        token,
+    let mut out_str = String::new();
+    out_str += &format!(
+        "\nUnexpected token '{}' at line {}, column {}",
+        file.chars().nth(err.location.offset).unwrap_or('\0'),
         err.location.line,
-        err.location.column,
-        err.expected
-            .tokens()
-            .map(|t| t.to_string())
-            .collect::<Vec<String>>()
-            .join(", ")
-    )
+        err.location.column
+    );
+    let line = file.split('\n').nth(err.location.line - 1).unwrap_or("\0");
+    out_str += &format!("\n|\n|  {}\n", line);
+    let mark_column = match err.location.column {
+        n if n < 1 => 0,
+        n => n - 1,
+    };
+    out_str += &format!("|~~{}^\n", "~".repeat(mark_column));
+
+    out_str
 }
 
 pub fn parse_string(s: &str) -> Result<ast::AST, String> {
