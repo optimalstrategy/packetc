9239147e6904935da465cbe3ab150f8eeb44fdb3
jprochazk <honza.spacir@gmail.com>
2020-12-30 03:29:40 +0100

    add some documentation to parser

--- src/lib/parser.rs
+++ src/lib/parser.rs
@@ -12,7 +12,7 @@ peg::parser!(pub grammar pkt() for str {
     rule comment()
         = "#" [ch if ch != '\n']* __
 
-    /// Parses reserved keywords
+    /// Parses reserved keywords (the base types)
     rule reserved()
         = "uint8"
         / "uint16"
@@ -26,10 +26,13 @@ peg::parser!(pub grammar pkt() for str {
     rule ident_start() -> String = s:$(['a'..='z'|'A'..='Z'|'_']) { s.to_string() }
     /// Parses any alphanumeric characters as part of an identifier
     rule ident_chars() -> String = s:$(['a'..='z'|'A'..='Z'|'0'..='9'|'_']) { s.to_string() }
-    /// Parses a single identifier
+    /// Parses an entire identifier, ensuring no reserved keywords are used
     rule ident() -> String
         = i:quiet!{ $(!reserved() ident_start() ident_chars()*) } { i.to_string() }
 
+    /// Parses the array base type - this is different from r#type()
+    /// by not including array() in the descent list, which could
+    /// create an infinite loop
     rule array_type() -> ast::Type
         = f:flag() { f }
         / t:tuple() { t }
@@ -42,6 +45,7 @@ peg::parser!(pub grammar pkt() for str {
         / "float" { ast::Type::Float }
         / "string" { ast::Type::String }
 
+    /// Parses a array in the form `identifier: type[]`
     rule array() -> ast::Type
         = base:array_type() nesting:$("[]"+) {
             let mut root = ast::Type::Array{ r#type: Box::new(base) };
@@ -52,6 +56,7 @@ peg::parser!(pub grammar pkt() for str {
             root
         }
 
+    /// Parses a flag in the form `identifier: { VARIANT_A, VARIANT_B }`
     rule flag() -> ast::Type
         = "{" ___ variants:$(ident() ** (___ "," ___)) ___ "}" {
             ast::Type::Flag {
@@ -61,9 +66,13 @@ peg::parser!(pub grammar pkt() for str {
             }
         }
 
+    /// Parses a tuple element in the form `identifier: type`
+    /// it's separate because PEG doesn't allow binding inside of
+    /// nested expressions, so the binding is done here
     rule tuple_element() -> (String, ast::Type)
         = i:ident() _ ":" _ t:r#type() ___ ","? ___ { (i, t) }
 
+    /// Parses a tuple in the form
     rule tuple() -> ast::Type
         = "(" ___ elements:tuple_element()+ ___ ")" {
             ast::Type::Tuple { elements }
